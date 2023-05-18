a17c3926a757ce5b29f9ccbcd6b7ab36ad1b0a1d
Willian Vallecilla <williand9415@gmail.com>
2023-05-17 22:41:16 -0500

    Key events finished

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 import {digitalWatch,soundAlarm} from "./dom/digital_clock.js";
 import hamburgerMenu from "./dom/nav_hamburger.js";
-
+import { shortcuts, moveBall } from "./dom/key_events.js";
 const d = document;
 
 d.addEventListener('DOMContentLoaded', e => {
     hamburgerMenu(".hamburger",".nav",".nav__link");
     digitalWatch('.btn-start-watch', '.btn-stop-watch');
-    soundAlarm('.btn-start-alarm', '.btn-stop-alarm')
+    soundAlarm('.btn-start-alarm', '.btn-stop-alarm');
 });
+
+d.addEventListener('keydown',(e)=> {
+    shortcuts(e);
+    moveBall(e,'.ball','.table');
+
+})
+
 
 //const $button = document.getElementById('hamburger__nav'),
       //$nav = document.querySelector('.nav');
